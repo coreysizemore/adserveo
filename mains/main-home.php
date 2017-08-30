02b30715938f4312b5a2e22013400206ed654c3a
Corey Sizemore <coreysizemore@gmail.com>
2017-08-29 19:39:33 -0700

    8-29-17

--- mains/main-home.php
+++ mains/main-home.php
@@ -203,6 +203,30 @@
 
 </div>
 
+<div class="facebook_feed">
+	
+	<div class="container">
+		
+		<div class="row gutters">
+			
+			<div class="col_12">
+				
+				<div class="content">
+					
+					<h1>Follow Us On Facebook</h1>
+					
+					<?php echo do_shortcode( '[custom-facebook-feed]' ); ?>
+					
+				</div>
+				
+			</div>
+			
+		</div>
+		
+	</div>
+	
+</div>
+
 <?php
 	
 	if( get_field('accordion') ):
