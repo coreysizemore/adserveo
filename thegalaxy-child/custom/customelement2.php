e4c7a212c40c4c3c9083c52a3c2f2573755c5e36
Corey Sizemore <coreysizemore@gmail.com>
2018-10-16 13:31:01 -0700

    v2.0.7 Theme Updates

---
+++ thegalaxy-child/custom/customelement2.php
@@ -0,0 +1,23 @@
+<div class="facebook_feed">
+	
+	<div class="container">
+		
+		<div class="row gutters">
+			
+			<div class="col_12">
+				
+				<div class="content">
+					
+					<h1>Follow Us On Facebook</h1>
+					
+					<?php echo do_shortcode( '[custom-facebook-feed]' ); ?>
+					
+				</div>
+				
+			</div>
+			
+		</div>
+		
+	</div>
+	
+</div>
--- thegalaxy-child/mains/main-home.php
+++ thegalaxy-child/mains/main-home.php
@@ -4,285 +4,190 @@
 		@package WordPress
 		@subpackage thegalaxy
 	*/
+	
+	if( get_field('secondary_navigation') == 'display' ):
+	
+		if ( has_nav_menu( 'secondary_nav' ) ):
+		
+	    	echo get_template_part( 'misc/secondarynav' );
+	    	    	
+	    endif;
+    
+    endif;
+    
+    if ( is_active_sidebar( 'attention-sidebar-1' ) ):
 	 
-?>
+		get_template_part( 'sidebars/sidebar' , 'attentiontop' );
 	
-<?php get_template_part( 'sidebars/sidebar' , 'announcement' ); ?>
+	endif;
 
-<?php 
+	if( get_field('display_call_out_boxes') == 'display' ):
 	
-	if( get_field('display_call_out_boxes') ):
-
 		get_template_part( 'misc/calloutboxes' );
-		
+	
 	endif;
-		
-?>
-
-<?php
-		
-	if( have_rows('sponsors','option') ):
+	
+	if( get_field('select_a_custom_element') != 'none' && get_field('custom_element_location') == 'above' ):
 			
-		echo '<div class="sponsor_wrapper"><div class="container"><div class="row gutters"><div class="col_12">';
+		get_template_part( 'custom/customelements' );
 				
-			if( get_field('sponsors_section_heading') ) :
+	endif;
+
+	echo '<div id="main" class="';
+	
+	echo basename(get_permalink());
+	
+	echo '">';
+	
+	if( get_field('default_editor') == 'display'):
+	
+		echo '<div class="default_editor"><div class="container"><div class="row gutters">';
+	
+		if( get_field('sidebar_selection') == 'right' ):
+	
+			echo '<div class="col_8"><div class="content">';
+	
+			get_template_part( 'loops/loop', 'page' );
+			
+			if( get_field('select_a_custom_element') != 'none' && get_field('custom_element_location') == 'inside' ):
+			
+				get_template_part( 'custom/customelements' );
 				
-				echo '<div class="content"><h2>';
-					
-				the_field('sponsors_section_heading');
-						
-				echo '</h2></div></div></div><div class="row gutters"><div class="col_12">';
-						
 			endif;
-						
-		while ( have_rows('sponsors','option') ) : the_row();
-						        
-			echo '<div class="sponsor_logo">';
-					
-			if( get_sub_field('link') ):
-					
-				echo '<a href="';
-							        
-				the_sub_field('link');
-							
-				echo '" target="_blank">';
-							
-				echo '<img src="';
-							
-				the_sub_field('image');
-							        
-				echo '" /></a>';
-						
-			else :
-					
-				echo '<img src="';
-							
-				the_sub_field('image');
-							        
-				echo '" /></a>';
-					
+
+			if( have_rows('gallery_content') ):
+			
+				get_template_part( 'misc/gallery' );
+				
 			endif;
-					
+
+			if( have_rows('accordion_content') ):
+			
+				get_template_part( 'misc/accordion' );
+				
+			endif;
+
+			if( have_rows('tabs_content') ):
+			
+				get_template_part( 'misc/tabs' );
+				
+			endif;
+
+			echo '</div></div><div class="col_4">';
+			
+			get_template_part( 'sidebars/sidebar' , 'primary' );
+			
 			echo '</div>';
-						
-		endwhile;
-					
-		echo '</div></div></div></div>';
-							
-	else :
+		
+		endif;
+	
+		if( get_field('sidebar_selection') == 'none' ):
+		
+			echo '<div class="col_12"><div class="content">';
+			
+			get_template_part( 'loops/loop', 'page' );
+			
+			if( get_field('select_a_custom_element') != 'none' && get_field('custom_element_location') == 'inside' ):
+			
+				get_template_part( 'custom/customelements' );
+				
+			endif;
+			
+			if( have_rows('gallery_content') ):
+			
+				get_template_part( 'misc/gallery' );
+				
+			endif;
+
+			if( have_rows('accordion_content') ):
+			
+				get_template_part( 'misc/accordion' );
+				
+			endif;
+
+			if( have_rows('tabs_content') ):
+			
+				get_template_part( 'misc/tabs' );
+				
+			endif;
+		
+			echo '</div></div>';
+		
+		endif;
+		
+		if( get_field('sidebar_selection') == 'left' ):
+		
+			echo '<div class="col_4">';
+			
+			get_template_part( 'sidebars/sidebar' , 'primary' );
+			
+			echo '</div><div class="col_8"><div class="content">';
+			
+			get_template_part( 'loops/loop', 'page' );
+			
+			if( get_field('select_a_custom_element') != 'none' && get_field('custom_element_location') == 'inside' ):
+			
+				get_template_part( 'custom/customelements' );
+				
+			endif;
+		
+			if( have_rows('gallery_content') ):
+			
+				get_template_part( 'misc/gallery' );
+				
+			endif;
+
+			if( have_rows('accordion_content') ):
+			
+				get_template_part( 'misc/accordion' );
+				
+			endif;
+
+			if( have_rows('tabs_content') ):
+			
+				get_template_part( 'misc/tabs' );
+				
+			endif;
+			
+			echo '</div></div>';
+		
+		endif;
+		
+		echo '</div></div></div>';
+	
+	endif;
+	
+	if(is_user_logged_in()):
+	
+		echo '<div class="edit_button"><span class="edit">';
+		
+		edit_post_link();
+		
+		echo '</span></div>';
+	
+	endif;
+	
+	echo '</div>';
+	
+	/* THIS IS EDITED SPECIFICALLY FOR THIS WEBSITE HOME PAGE */
+	
+	if( get_field('select_a_custom_element') != 'none' ):
+			
+		get_template_part( 'custom/customelement2' );
 				
 	endif;
-			
+	
+	/* END CUSTOM EDITS */
+	
+	get_template_part( 'misc/parallax' );
+
+	if( get_field('attention_bottom' ) == 'display'):
+	
+		if ( is_active_sidebar( 'attention-sidebar-2' ) ):
+	 
+			get_template_part( 'sidebars/sidebar' , 'attentionbottom' );
+		
+		endif;
+		
+	endif;
 ?>
-
-<div id="main" class="<?php echo basename(get_permalink()); ?>">
-
-	<?php if( get_field('default_editor')): ?>
-	
-		<div class="default_editor">
-		
-			<div class="container">
-					
-				<div class="row gutters">
-						
-					<?php if( get_field('sidebar_selection') == 'right' ): ?>
-						
-						<div class="col_9">
-								
-							<div class="content">
-					
-								<?php get_template_part( 'loops/loop', 'page' ); ?>
-								
-								<?php
-	
-									if( get_field('gallery') ):
-								
-										get_template_part( 'misc/gallery' );
-										
-									endif;
-										
-								?>
-								
-								<?php 
-									
-									if( get_field('accordion') ):
-								
-										get_template_part( 'misc/accordion' );
-										
-									endif;
-										
-								?>
-								
-								<?php 
-									
-									if( get_field('tabs') ):
-								
-										get_template_part( 'misc/tabs' );
-										
-									endif;
-										
-								?>
-									
-							</div>
-								
-						</div>
-							
-						<div class="col_3">
-								
-							<?php get_template_part( 'sidebars/sidebar' , 'primary' ); ?>
-								
-						</div>
-					
-					<?php endif; ?>
-					
-					<?php if( get_field('sidebar_selection') == 'none' ): ?>
-					
-						<div class="col_12">
-							
-							<div class="content">
-				
-								<?php get_template_part( 'loops/loop', 'page' ); ?>
-								
-								<?php
-	
-									if( get_field('gallery') ):
-								
-										get_template_part( 'misc/gallery' );
-										
-									endif;
-										
-								?>
-								
-								<?php 
-									
-									if( get_field('accordion') ):
-								
-										get_template_part( 'misc/accordion' );
-										
-									endif;
-										
-								?>
-								
-								<?php 
-									
-									if( get_field('tabs') ):
-								
-										get_template_part( 'misc/tabs' );
-										
-									endif;
-										
-								?>
-								
-							</div>
-							
-						</div>
-					
-					<?php endif; ?>
-					
-					<?php if( get_field('sidebar_selection') == 'left' ): ?>
-					
-						<div class="col_3">
-								
-							<?php get_template_part( 'sidebars/sidebar' , 'primary' ); ?>
-							
-						</div>
-						
-						<div class="col_9">
-							
-							<div class="content">
-				
-								<?php get_template_part( 'loops/loop', 'page' ); ?>
-								
-								<?php
-	
-									if( get_field('gallery') ):
-								
-										get_template_part( 'misc/gallery' );
-										
-									endif;
-										
-								?>
-								
-								<?php 
-									
-									if( get_field('accordion') ):
-								
-										get_template_part( 'misc/accordion' );
-										
-									endif;
-										
-								?>
-								
-								<?php 
-									
-									if( get_field('tabs') ):
-								
-										get_template_part( 'misc/tabs' );
-										
-									endif;
-										
-								?>
-								
-							</div>
-							
-						</div>
-					
-					<?php endif; ?>
-						
-				</div>
-				
-			</div>
-			
-		</div>
-	
-	<?php endif; ?>
-	
-	<?php if(is_user_logged_in()):?>
-	
-		<div class="edit_button">
-		
-			<span class="edit"><?php edit_post_link(); ?></span>
-		
-		</div>
-	
-	<?php endif; ?>
-
-</div>
-
-<div class="facebook_feed">
-	
-	<div class="container">
-		
-		<div class="row gutters">
-			
-			<div class="col_12">
-				
-				<div class="content">
-					
-					<h1>Follow Us On Facebook</h1>
-					
-					<?php echo do_shortcode( '[custom-facebook-feed]' ); ?>
-					
-				</div>
-				
-			</div>
-			
-		</div>
-		
-	</div>
-	
-</div>
-
-<?php get_template_part( 'misc/parallax' ); ?>
-
-<?php
-
-	if(get_field('appointment_feature'))
-	{
-		get_template_part( 'sidebars/sidebar' , 'appointment' );
-	}
-						
-?>
-	
-	
-	
